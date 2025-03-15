6bdacc45ec76ef4f5835295c79c1b84c95ade499
BoaN235 <Noahefort@gmail.com>
2025-03-15 17:48:19 -0400

    feat: remove unused image assets and add new QuestionMark texture

--- Cards/Scripts/p_2.gd
+++ Cards/Scripts/p_2.gd
@@ -3,17 +3,20 @@ extends Node2D
 @onready var panel = get_node("Panel")
 @onready var image = get_node("Image").get_child(0)
 var offset = Vector2()
+var orignal_pos
 
 func _on_button_button_down() -> void:
 	panel.visible = true
 	image.z_index =+ 1;
 	panel.z_index =+ 1;
+	orignal_pos = position
 	offset = position - get_global_mouse_position()
 
 func _on_button_button_up() -> void:
 	panel.visible = false
 	image.z_index =- 1;
 	panel.z_index =- 1;
+	position = orignal_pos
 
 func _process(delta: float) -> void:
 	if panel.visible == true:
--- Scripts/run.gd
+++ Scripts/run.gd
@@ -1,9 +1,22 @@
 extends Node
 
 
+
 @onready var map = get_node("Maps")
 
 func _ready():
-	var maps = map.get_children()
-	for map in maps:
-		map.text = "?"
+	var rng = RandomNumberGenerator.new()
+	for i in range(6):
+		var my_random_number = rng.randf_range(200.0, 900.0)
+		var my_random_number2 = rng.randf_range(100.0, 500.0)
+		
+		# Create a new Sprite2D node
+		var new_sprite = Sprite2D.new()
+
+		# Set the sprite's texture (optional)
+		new_sprite.texture = preload("res://Assets/QuestionMark.png")
+
+		new_sprite.position = Vector2(my_random_number, my_random_number2)
+
+		# Add the sprite as a child of the current node
+		add_child(new_sprite)
