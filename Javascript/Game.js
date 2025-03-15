0a80d85725bc357404ed26a2d53d47393713b269
BoaN235 <Noahefort@gmail.com>
2025-03-15 15:11:12 -0400

    refactor: remove obsolete game files and assets

--- Javascript/Game.js
+++
@@ -1,6 +0,0 @@
-import { Deck } from './PlayerScripts/DeckMech/Deck.js';
-import { Player } from './PlayerScripts/Player.js';
-import { Card, AttackCard, HealCard } from './Cards.js';
-
-// Example usage
-const player = new Player('John Doe');
