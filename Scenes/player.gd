1911f3e475aad3cd5fd1a9b7d4c0611b5d8f8d4f
BoaN235 <Noahefort@gmail.com>
2025-03-16 00:50:31 -0400

    feat: refactor card scene and script structure, add new P3 scene

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,3 +3,17 @@ extends Node2D
 var HP = Stats.stats["HP"]
 var strength = Stats.stats["strength"]
 var Nvp = Stats.stats["Nvp"]
+var hand
+
+func _ready() -> void:
+	for i in range(5):
+		draw()
+
+func draw():
+	pass
+
+func discard():
+	pass
+
+func start():
+	pass
